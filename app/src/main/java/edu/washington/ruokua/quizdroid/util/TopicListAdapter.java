4e80d95582324f22e75136883eeca46ccc20ca8d
Ruokun An <ruokua@cs.washington.edu>
2015-05-09 14:00:45 -0700

    change layout for the topic list in topic-list_item.xml

--- app/src/main/java/edu/washington/ruokua/quizdroid/util/TopicListAdapter.java
+++ app/src/main/java/edu/washington/ruokua/quizdroid/util/TopicListAdapter.java
@@ -1,7 +1,24 @@
 package edu.washington.ruokua.quizdroid.util;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
 /**
  * Created by ruokua on 5/8/15.
  */
-public class TopicListAdapter {
+public class TopicListAdapter extends ArrayAdapter<String>{
+    private final Context context;
+    private final List<String> topics;
+
+    /**
+     * {@inheritDoc}
+     *  Construct a list adpater  
+     */
+    public TopicListAdapter(Context context, int resource, List<String> topics) {
+        super(context, resource, topics);
+        this.context = context;
+        this.topics = topics;
+    }
 }
