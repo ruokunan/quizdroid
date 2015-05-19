fe93168c55ea54ccd30b18fef7489e18bd68aaef
Ruokun An <ruokua@cs.washington.edu>
2015-05-19 12:06:01 -0700

    the setting perence

--- app/src/main/java/edu/washington/ruokua/quizdroid/activity/PreferenceActivity.java
+++ app/src/main/java/edu/washington/ruokua/quizdroid/activity/PreferenceActivity.java
@@ -1,17 +1,43 @@
 package edu.washington.ruokua.quizdroid.activity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import edu.washington.ruokua.quizdroid.R;
+import edu.washington.ruokua.quizdroid.fragment.PreferenceSettingsFragment;
 
 /**
  * Created by ruokua on 5/16/15.
  */
-public class PreferenceActivity {
+public class PreferenceActivity  extends AppCompatActivity   {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new PreferenceSettingsFragment())
+                .commit();
+    }
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_perference, menu);
+        return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()== R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
